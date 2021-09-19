28d51cac18e776a7a5ef44228f96b2c4b7df1972
devsh0 <devashishjaiswal86@gmail.com>
2021-09-19 22:27:28 +0530

    Messing around with the overlay

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ document.querySelectorAll(".mobile-navigation li").forEach(nav => {
        previousFocus = nav;
    }
 })
+
+let dummyCheckbox = document.querySelector("#dummy");
+let mobileNav = document.querySelector(".mobile-navigation");
+let overlay = document.querySelector("#overlay");
+dummyCheckbox.onclick = () => {
+    if (dummyCheckbox.checked) {
+        mobileNav.classList.add("active")
+        overlay.classList.add("active");
+    }
+    else {
+        mobileNav.classList.remove("active");
+        overlay.classList.remove("active");
+    }
+}
